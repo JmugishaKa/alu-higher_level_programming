390d3d725f3361a08ef9f064f47d1ebbb5a2f46f
JmugishaKa <j.mugisha2@alustudent.com>
2024-10-24 09:43:55 -0700

    100-write.py

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-print("and that piece of art is useful - Dora Korpar, 2015-10-19", file=sys.stderr)
+print("and that piece of art is useful - Dora Korpar, 2015-10-19 (written)", file=sys.stderr)
 sys.exit(1)
 
