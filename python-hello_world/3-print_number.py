849e004c297b494fbb7df9fc4bd823bc4dfaa909
JmugishaKa <j.mugisha2@alustudent.com>
2024-10-24 08:56:42 -0700

    3-print_number.py

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-number = 98; output =(f"The number {number} is {'Positive' if number > 0 else 'Zero' if number == 0 else 'Negative'}. " \
-         f"If it were zero: 'Zero', if it were negative: 'Negative'.98 Battery street") 
-print(output)
-
-
-
+number = 98
+print(f"{number:d} Battery street")
