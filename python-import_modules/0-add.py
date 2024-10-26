46bc35b248dd55e356461058b55a50c5acbd56a9
JmugishaKa <j.mugisha2@alustudent.com>
2024-10-26 07:10:03 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def add(a, b):
-    """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
     return (a + b)
+
+if __name__ == "__main__":
+    result = add(1, 2)
+    print(f"1 + 2 = {result}")
