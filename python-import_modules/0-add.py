9a000040417d339ae86057be1c92269922038ff7
JmugishaKa <j.mugisha2@alustudent.com>
2024-10-26 07:54:19 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-
-    return (a + b)
-
 if __name__ == "__main__":
-    result = add(1, 2)
-    print(f"1 + 2 = {result}")
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+
