3e558480d63a58895793b95769d11c0dceb1c9ff
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-15 14:26:38 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''A python script that can execute the contents of a file'''
+
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout."""
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    '''Reads the data from outside file '''
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
