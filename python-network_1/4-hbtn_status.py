950d8e88787931c721f00e2f3f9fd685ebb7d68f
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 16:55:58 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text.strip()))
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(response.text), 
+        "OK" if response.status_code == 200 
+        else response.text))
