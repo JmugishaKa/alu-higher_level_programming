6164910ba5a3415dde72dbb81d1a52e6ed6b2461
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 16:38:10 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("http://0.0.0.0:5050/status")
-    print("Body response:")
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
