650b66ba23b7180efaddb0f4b11c4884b5aff772
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 15:26:49 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status."""
+"""
+Fetches https://alu-intranet.hbtn.io/status and handles potential HTTP errors.
+"""
 
-if __name__ == '__main__':
-    import urllib.request
-    from urllib.error import HTTPError
+from urllib import request, error
+
+url = "https://alu-intranet.hbtn.io/status"
+
+if __name__ == "__main__":
+    # Add a custom User-Agent header
+    req = request.Request(url, headers={"User-Agent": "Python-urllib/3.10"})
 
     try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-            content = resp.read()
+        with request.urlopen(req) as response:
+            content = response.read()
             print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except HTTPError as e:
-        print("[Got]")
-        print("HTTP Error: {}".format(e))
-        print("(38 chars long)")
-        print("[stderr]:")
-        print("(0 chars long)")
-        print("[Expected]")
-        print("Body response:")
-        print("\t- type: <class 'bytes'>")
-        print("\t- content: b'OK'")
-        print("\t- utf8 content: OK")
-        print("(78 chars long)")
-        print("[stderr]: [Anything]")
-        print("(0 chars long)")
-        if e.code == 403:
-            print("Access to the resource is forbidden.")
+            print(f"    - type: {type(content)}")
+            print(f"    - content: {content}")
+            print(f"    - utf8 content: {content.decode('utf-8')}")
+    except error.HTTPError as e:
+        print(f"HTTP Error: {e}")
+    except error.URLError as e:
+        print(f"URL Error: {e}")
+
