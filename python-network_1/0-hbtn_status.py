ee47f4e093a4525023dd7ad42ebdb45caaecfa94
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 18:37:58 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status."""
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
-    from urllib.error import HTTPError
 
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-            content = resp.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except HTTPError as e:
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: <class 'bytes'>")
-        print("\t- content: b'OK'")
-        print("\t- utf8 content: OK")
-        print("(78 chars long)")
-        print("[stderr]: [Anything]")
-        print("(0 chars long)")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
