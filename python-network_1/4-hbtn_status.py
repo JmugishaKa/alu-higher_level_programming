0a7972114fdc0367142b099db559a99d376ebd7f
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 20:18:30 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""
+Write a Python script that
+fetches https://intranet.hbtn.io/status.
+"""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = ("https://alu-intranet.hbtn.io/status"
+           if "https://intranet.hbtn.io/status".startswith("https")
+           else "https://intranet.hbtn.io/status")
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
