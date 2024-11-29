6eaa6c6564cd17981b7eced2050230ea03c5efc7
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 17:01:31 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,11 +2,15 @@
 """Fetch a url"""
 
 import requests
-
-
     
 if __name__ == "__main__":
     response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text.strip()))
+
+    # Extract "OK" from the response text
+    if "OK" in response.text:
+        print("\t- content: OK")
+    else:
+        print("\t- content: Not Found")
