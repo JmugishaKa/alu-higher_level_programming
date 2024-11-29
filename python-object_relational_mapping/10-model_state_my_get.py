6d94e711f47bf67dbb4fca267ad30b5303364f8e
JmugishaKa <j.mugisha2@alustudent.com>
2024-11-29 14:24:57 +0200

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -6,12 +6,12 @@ Writes out the State object with the name passed as argument from the db
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine  #This ensure that the create engine is imported.
+from sqlalchemy.orm import sessionmaker # The session maker is being imported.
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'. # Directs the creation of engine creation.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
